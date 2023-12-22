4f42631938a0664b7564050245e2e6b1cbb07558
Chandru <86843423+chandru-engineer@users.noreply.github.com>
2023-12-22 10:34:17 +0530

    Update check.py
    Signed-off-by: Chandru <86843423+chandru-engineer@users.noreply.github.com>

--- check.py
+++ check.py
@@ -2,38 +2,54 @@ import os
 import requests
 
 path = os.path.abspath(os.curdir)
-odoo_version = "14.0"
+odoo_versions = ['9.0', '10.0', '11.0', '12.0', '13.0', '14.0', '15.0', '16.0', '17.0']
 
-def appendFile(module_name, upgradable):
+def append_file(module_name, upgradable, index):
     with open("./README.md", "a") as readme:
-        if (upgradable[0] == "true"):
-            readme.write("[%s](%s/) | %s | [%s](%s) \n"%(module_name, module_name, upgradable[0], module_name, upgradable[1]))
-        else:
-            readme.write("[%s](%s/) | %s | -- \n"%(module_name, module_name, upgradable[0]))
+        readme.write(f"{index}. [{module_name}]({module_name}/) | ")
+        for odoo_version in odoo_versions:
+            supported, link = check_upgrade(module_name, odoo_version)
+            if supported == "true":
+                readme.write(f"[True]({link}) | ")
+            else:
+                readme.write("False | ")
+        readme.write("\n")
 
-def listdirs(path):
-    return [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
-    
-def checkUpgrade(module_name, odoo_version):
-    response = requests.get("https://apps.odoo.com/apps/modules/%s/%s/"%(odoo_version, module_name))
-    if (response.status_code == 200):
-        return "true", "https://apps.odoo.com/apps/modules/%s/%s/"%(odoo_version, module_name)
-    elif (response.status_code == 404):
+def list_dirs(path):
+    return [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d)) and not d.startswith('.git')]
+
+def check_upgrade(module_name, odoo_version):
+    response = requests.get(f"https://apps.odoo.com/apps/modules/{odoo_version}/{module_name}/")
+    if response.status_code == 200:
+        return "true", f"https://apps.odoo.com/apps/modules/{odoo_version}/{module_name}/"
+    elif response.status_code == 404:
         return "false", "Not Found"
 
-def deleteLines():
-    with open("./README.md", "r") as input:
-        with open("./README-temp.md", "w") as output:
-            for line in input:
+def delete_lines():
+    with open("./README.md", "r") as input_file:
+        with open("./README-temp.md", "w") as output_file:
+            for line in input_file:
                 if not line.strip("\n").startswith('['):
-                    output.write(line)
+                    output_file.write(line)
 
 print("Starting to check!")
 
-deleteLines()
+delete_lines()
 os.replace('./README-temp.md', './README.md')
-modules = listdirs(path)
-for module in modules:
-    appendFile(module, checkUpgrade(module, odoo_version))
+
+# Header row
+with open("./README.md", "a") as readme:
+    readme.write("Index | Addon | ")
+    for odoo_version in odoo_versions:
+        readme.write(f"Upgradeable to {odoo_version} | ")
+    readme.write("\n--- | --- | ")
+    for _ in odoo_versions:
+        readme.write("--- | ")
+    readme.write("\n")
+
+modules = list_dirs(path)
+for i, module in enumerate(modules, start=1):
+    print(f"Processing {module}...")
+    append_file(module, check_upgrade(module, odoo_version), i)
 
 print("Finished checking!")
